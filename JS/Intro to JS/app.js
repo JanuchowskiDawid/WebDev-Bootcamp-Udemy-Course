ad274af9e143b476287ea0e469b1420fab7f6bae
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-04 23:29:50 +0200

    First commands in js

---
+++ JS/Intro to JS/app.js
@@ -0,0 +1,11 @@
+//console printing
+console.log("Hello World?");
+let total = 1 + 3;
+console.log(total);
+//if-else statemets
+let random = Math.random();
+if (random < 0.5) {
+    console.log("random is less then 0.5");
+} else {
+    console.log("random is greater then 0.5");
+}
