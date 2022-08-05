60475df28d1df8d0ac0511ef437f2785fffa07bd
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-05 17:04:49 +0200

    Finished logical section

--- JS/Intro to JS/app.js
+++ JS/Intro to JS/app.js
@@ -1,24 +1,58 @@
-//console printing
-console.log("Hello World?");
-let total = 1 + 3;
-console.log(total);
-//if-else statemets
-let random = Math.random();
-if (random < 0.5) {
-    console.log("random is less then 0.5");
+// //console printing
+// console.log("Hello World?");
+// let total = 1 + 3;
+// console.log(total);
+// //if-else statemets
+// let random = Math.random();
+// if (random < 0.5) {
+//     console.log("random is less then 0.5");
+// } else {
+//     console.log("random is greater then 0.5");
+// }
+// //nested conditional
+// const password = prompt("please enter a new password");
+
+// if (password.length >= 6) {
+//     if (password.indexOf(" ") === -1) {
+//         console.log("Valid password");
+//     } else {
+//         console.log("remove that space!");
+//     }
+
+// } else {
+//     console.log("too short");
+// }
+const userInput = prompt("enter smt");
+if (userInput) {
+    console.log("true");
 } else {
-    console.log("random is greater then 0.5");
+    console.log("false");
 }
-//nested conditional
-const password = prompt("please enter a new password");
+const day = 2;
 
-if (password.length >= 6) {
-    if (password.indexOf(" ") === -1) {
-        console.log("Valid password");
-    } else {
-        console.log("remove that space!");
-    }
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Not a Monday");
+        break;
+    case 3:
+        console.log("Not a Monday");
+        break;
+    case 4:
+        console.log("Not a Monday");
+        break;
+    case 5:
+        console.log("Not a Monday");
+        break;
+    case 6:
+        console.log("Not a Monday");
+        break;
+    case 7:
+        console.log("Not a Monday");
+        break;
+    default:
+        console.log("Not a day");
 
-} else {
-    console.log("too short");
 }
