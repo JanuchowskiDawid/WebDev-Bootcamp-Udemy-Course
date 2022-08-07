2c12763076109a357bf307732330e29e5783625f
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-07 14:32:44 +0200

    Filter function

--- JS/Intro to JS/exercises.js
+++ JS/Intro to JS/exercises.js
@@ -170,3 +170,20 @@ let firstNames = fullNames.map(function (fullNamesObj) {
 const greet = (name) => {
     return `Hey ${name}!`;
 };
+
+// Filter Exercise
+// Let's get some practice using the filter method. Write a function called validUserNames that accepts an array of usernames (strings).  It should return a new array, containing only the usernames that are less than 10 characters. For example:
+
+// validUserNames(['mark', 'staceysmom1978', 'q29832128238983', 'carrie98', 'MoanaFan']);
+// // => ["mark", "carrie98", "MoanaFan"]
+
+
+// Note: The syntax for this solution might be a little strange to you at this point in the course because it requires you to write the code, that you just learned in the previous lecture, inside of a function. e.g.,
+
+// function validUserNames(usernames) {
+//   // your code here
+// }
+
+function validUserNames(usernames) {
+    return usernames.filter((username) => username.length < 10);
+}
