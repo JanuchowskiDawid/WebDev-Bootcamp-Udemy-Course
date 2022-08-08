a84edcbe89fcb8686d67939b99500b566b30b550
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-08 14:10:58 +0200

    Changed images by selecting by tags and class names

--- JS/Selecting/app.js
+++ JS/Selecting/app.js
@@ -1,14 +1,14 @@
 const allImages = document.getElementsByTagName('img');
 
 for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+    img.src = 'imgs/gumball.png'
 }
 
 
 const squareImages = document.getElementsByClassName('square');
 
 for (let img of squareImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg';
+    img.src = 'imgs/hand2.png';
 }
 
 const links = document.querySelectorAll('p a');
