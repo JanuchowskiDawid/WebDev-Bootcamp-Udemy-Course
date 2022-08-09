bfeb16f67874caf200cd84048dfbde9a414b58c1
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-09 11:45:44 +0200

    Adding elements to page via JS

--- JS/Manipulating/exercises.js
+++ JS/Manipulating/exercises.js
@@ -27,3 +27,19 @@ const liLinks = document.querySelectorAll('li');
 for (let link of liLinks) {
     link.classList.toggle('highlight');
 }
+
+// 100 Button Insanity Exercise
+// Without touching index.html, please use JavaScript to create exactly 100 new button elements! Add each new button inside the container element provided in index.html.  Unfortunately, Udemy's exercise tool does not support to the append method, so you will need to use appendChild.  Here are the steps:
+
+// Create exactly 100 new button elements
+
+// Each button must have some text inside of it (it doesn't matter what)
+
+// Each button must be appended inside the container div.
+const divElement = document.querySelector("div");
+let buttons = [];
+for (let i = 0; i < 100; i++) {
+    buttons[i] = document.createElement('button');
+    buttons[i].innerText = 'Hey!';
+    divElement.appendChild(buttons[i]);
+}
