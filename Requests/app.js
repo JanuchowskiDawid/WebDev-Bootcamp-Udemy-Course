2fecb532ac2e9c4d0c8b070c426ee37b9cc23014
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-12 14:26:17 +0200

    Created request via fetch

--- Requests/app.js
+++ Requests/app.js
@@ -1,16 +1,34 @@
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-req.onload = function () {
-    console.log("succes");
-    const data = JSON.parse(this.responseText);
-    console.log(data);
-}
+// req.onload = function () {
+//     console.log("succes");
+//     const data = JSON.parse(this.responseText);
+//     console.log(data);
+// }
 
-req.onerror = function () {
-    console.log("error");
-    console.log(this);
-}
+// req.onerror = function () {
+//     console.log("error");
+//     console.log(this);
+// }
 
-req.open("GET", "https://swapi.dev/api/people/1/");
-req.send();
-
+// req.open("GET", "https://swapi.dev/api/people/1/");
+// req.send();
+fetch("https://swapi.dev/api/people/1/")
+    .then(res => {
+        console.log("RESOLVED", res);
+        return res.json();
+    })
+    .then((data => {
+        console.log(data.name);
+        return fetch("https://swapi.dev/api/people/2/")
+    }))
+    .then(res => {
+        return res.json();
+    })
+    .then((data => {
+        console.log("SECOUND REQUEST RESOLVED!");
+        console.log(data.name);
+    }))
+    .catch((e) => {
+        console.log("ERROR", e);
+    })
