2d6946cb592fdd97161af7951e43e90f75a209a5
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-13 14:32:44 +0200

    request using async

--- Requests/app.js
+++ Requests/app.js
@@ -13,22 +13,36 @@
 
 // req.open("GET", "https://swapi.dev/api/people/1/");
 // req.send();
-fetch("https://swapi.dev/api/people/1/")
-    .then(res => {
-        console.log("RESOLVED", res);
-        return res.json();
-    })
-    .then((data => {
+// fetch("https://swapi.dev/api/people/1/")
+//     .then(res => {
+//         console.log("RESOLVED", res);
+//         return res.json();
+//     })
+//     .then((data => {
+//         console.log(data.name);
+//         return fetch("https://swapi.dev/api/people/2/")
+//     }))
+//     .then(res => {
+//         return res.json();
+//     })
+//     .then((data => {
+//         console.log("SECOUND REQUEST RESOLVED!");
+//         console.log(data.name);
+//     }))
+//     .catch((e) => {
+//         console.log("ERROR", e);
+//     })
+
+const loadStarWarsPPL = async () => {
+    try {
+        const res = await fetch("https://swapi.dev/api/people/1/");
+        const data = await res.json();
         console.log(data.name);
-        return fetch("https://swapi.dev/api/people/2/")
-    }))
-    .then(res => {
-        return res.json();
-    })
-    .then((data => {
-        console.log("SECOUND REQUEST RESOLVED!");
-        console.log(data.name);
-    }))
-    .catch((e) => {
-        console.log("ERROR", e);
-    })
+        const res2 = await fetch("https://swapi.dev/api/people/2/");
+        const data2 = await res2.json();
+        console.log(data2.name);
+    } catch (e) {
+        console.log(e);
+    }
+}
+loadStarWarsPPL();
