c7492ec1d3266543396a050e0ada700e1eaa2d3f
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-15 12:25:00 +0200

    Created Constructor function and added prototype method to this

--- Prototypes/app.js
+++ Prototypes/app.js
@@ -1,3 +1,21 @@
 String.prototype.yell = function () {
     console.log(this.toUpperCase());
 }
+
+function Player(height, weight, skill) {
+    this.weight = weight;
+    this.height = height;
+    this.skill = skill;
+}
+
+Player.prototype.slide = function () {
+    const skill = this.skill;
+    if (skill < 3) return;
+    console.log("Success!");
+}
+
+const messi = new Player(169, 67, 5);
+messi.slide();
+
+const almostMessi = new Player(169, 67, 5);
+almostMessi.slide();
