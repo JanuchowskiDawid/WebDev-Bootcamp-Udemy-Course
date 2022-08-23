aa2d3965da873fc93568588d1c87b94297fb88e4
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-23 19:51:17 +0200

    Created file math.js and used it in another file app.js

---
+++ NODE/math.js
@@ -0,0 +1,14 @@
+const add = (x, y) => x + y;
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+
+
+exports.PI = PI;
+exports.square = square;
+exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+// module.exports.add = add;
