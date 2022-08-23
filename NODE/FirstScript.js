474338f543e7e8d34a15e7d0a9e56a957373e090
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-23 06:15:46 +0200

    First script with node

---
+++ NODE/FirstScript.js
@@ -0,0 +1,16 @@
+for (let i = 0; i < 10; i++) {
+    console.log("Hello World!");
+}
+
+//result
+// $ node FirstScript.js
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
+// Hello World!
