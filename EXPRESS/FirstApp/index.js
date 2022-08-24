b207ba57eea23ebd350c98aca4d52ce6de54c6f8
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-24 17:50:17 +0200

    Server has responses to 3 different paths and default

--- EXPRESS/FirstApp/index.js
+++ EXPRESS/FirstApp/index.js
@@ -2,11 +2,25 @@ const express = require('express');
 const app = express();
 console.dir(app);
 
-app.use((req, res) => {
-    console.log("new request");
-    res.send("Hello, we got your request");
+// app.use((req, res) => {
+//     console.log("new request");
+//     res.send("Hello, we got your request");
+// })
+
+app.get('/cat', (req, res) => {
+    res.send('meow');
 })
 
+app.get('/dog', (req, res) => {
+    res.send('woof');
+})
+
+app.get('/', (req, res) => {
+    res.send('home page');
+})
+app.get('*', (req, res) => {
+    res.send("don't know that path");
+})
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000!");
 });
