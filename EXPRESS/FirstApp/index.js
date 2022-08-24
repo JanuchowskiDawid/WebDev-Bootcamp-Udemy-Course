8096fd171c8d15edfb677671053d7f2946adcc00
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-24 23:52:33 +0200

    Working with params of request

--- EXPRESS/FirstApp/index.js
+++ EXPRESS/FirstApp/index.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
-console.dir(app);
 
 // app.use((req, res) => {
 //     console.log("new request");
 //     res.send("Hello, we got your request");
 // })
+
+app.get('/', (req, res) => {
+    res.send('This is home page!');
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const subreddit = req.params.subreddit;
+    res.send(`This is ${subreddit}`);
+})
+
+app.get('/r/:subreddit/:postID', (req, res) => {
+    const subreddit = req.params.subreddit;
+    const post = req.params.postID;
+
+    res.send(`This is ${subreddit}. Wieving PostID: ${post}`);
+})
 
 app.get('/cat', (req, res) => {
     res.send('meow');
@@ -15,12 +30,10 @@ app.get('/dog', (req, res) => {
     res.send('woof');
 })
 
-app.get('/', (req, res) => {
-    res.send('home page');
-})
-app.get('*', (req, res) => {
-    res.send("don't know that path");
-})
 app.listen(3000, () => {
     console.log("LISTENING ON PORT 3000!");
 });
+
+app.get('*', (req, res) => {
+    res.send("don't know that path");
+})
