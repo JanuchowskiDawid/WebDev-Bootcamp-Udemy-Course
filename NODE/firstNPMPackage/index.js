4f8f0780613e94f2a3589c0a38c93c45e4948b4d
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-24 08:45:41 +0200

    Combined joke telling with cowsay, now cow says jokes

--- NODE/firstNPMPackage/index.js
+++ NODE/firstNPMPackage/index.js
@@ -1,6 +1,11 @@
 const jokes = require('give-me-a-joke');
 const colors = require('colors');
+const cowsay = require('cowsay');
 
 jokes.getRandomDadJoke(function (joke) {
-    console.log(joke.rainbow);
+    console.log(cowsay.say({
+        text: joke.rainbow,
+        e: "oO",
+        T: "U "
+    }));
 });
