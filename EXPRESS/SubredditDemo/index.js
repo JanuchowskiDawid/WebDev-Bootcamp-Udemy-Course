5b7df1699ccd9fdc387cea97478f4dcb6ad61536
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-25 21:09:06 +0200

    Created partials, included bootstrap navbar

--- EXPRESS/SubredditDemo/index.js
+++ EXPRESS/SubredditDemo/index.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const redditData = require('./data.json');
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.render('home');
+    res.render('home', { 'name': 'home' });
 })
 
 app.get('/r/:subreddit', (req, res) => {
