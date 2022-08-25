da9e9bf642efc88a63422df466f287ecaf0a3859
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-25 16:59:04 +0200

    Created new sites with EJS

--- EXPRESS/EJS/index.js
+++ EXPRESS/EJS/index.js
@@ -9,6 +9,16 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
+app.get('/rand', (req, res) => {
+    const rand = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { rand });
+})
+
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.render('subreddit', { subreddit });
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 })
