03f5bf1d820a2a39fa566a8ce94097d895fadffc
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-27 10:29:53 +0200

    Inserted new values to movies, updated and deleted via console

--- Mongoose/index.js
+++ Mongoose/index.js
@@ -8,3 +8,26 @@ mongoose.connect('mongodb://localhost:27017/movies', { useNewUrlParser: true, us
         console.log(err);
     })
 
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+// const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+// Movie.insertMany([
+//     { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+//     { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+//     { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+//     { title: 'Stand by me', year: 1986, score: 8.6, rating: 'R' },
+//     { title: 'Moonrise Kongdom', year: 2012, score: 7.3, rating: 'PG=13' }
+// ])
+//     .then((data) => {
+//         console.log('done');
+//         console.log(data);
+//     })
+//findOneAndDelete() -działa lepiej niż zwykły update, bo zwraca obiekt udatowany
+//findOneAndUpdate()-działa lepiej niż zwykły delete, bo zwraca obiekt usunięty
