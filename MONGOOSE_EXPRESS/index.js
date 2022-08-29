8069062d626fe55ed446d8651457eb8a033e7225
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-29 16:32:50 +0200

    Finished local shop app

--- MONGOOSE_EXPRESS/index.js
+++ MONGOOSE_EXPRESS/index.js
@@ -24,9 +24,14 @@ app.use(methodOverride('_method'))
 const categories = ['fruit', 'vegetable', 'dairy'];
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    console.log(products);
-    res.render('./products', { products });
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ catergory: category });
+        res.render('./products', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('./products', { products, category: 'All' });
+    }
 })
 
 app.get('/products/new', (req, res) => {
@@ -53,9 +58,14 @@ app.get('/products/:id', async (req, res) => {
 
 app.put('/products/:id', async (req, res) => {
     const { id } = req.params;
-    console.log(req.body);
     const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
     res.redirect(`/products/${id}`);
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+    res.redirect(`/products`);
 })
 
 app.listen(3000, () => {
