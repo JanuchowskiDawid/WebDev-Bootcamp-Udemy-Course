62e6582683e7aa8ee9328216871c6066d0a60ad2
Dawid Januchowski <mmalpa97@wp.pl>
2022-08-30 12:30:17 +0200

    Route protecting

--- morgan/index.js
+++ morgan/index.js
@@ -3,21 +3,40 @@ const app = express();
 const morgan = require('morgan');
 
 app.use(morgan('tiny'));
-app.use((req, res, next) => {
-    console.log('First Middleware');
-    next();
-})
-app.use((req, res, next) => {
-    console.log('Secound Middleware');
-    next();
+// app.use((req, res, next) => {
+//     // req.method = 'GET';
+//     req.requestTime = Date.now();
+//     console.log(req.method, req.path);
+//     next();
+// })
+
+const verifyPassword = (req, res, next) => {
+    const { password } = req.query;
+    if (password === 'nuggets') {
+        next();
+    }
+    res.send('Wrong password');
+
+}
+
+app.use('/dogs', (req, res, next) => {
+    console.log('middleware on every method with dogs');
 })
 
 app.get('/', (req, res) => {
     res.send('home');
 })
 
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('Secret is: ...');
+})
+
 app.get('/dog', (req, res) => {
     res.send('woof');
+})
+
+app.use((req, res) => {
+    res.send('404 not found');
 })
 
 app.listen(3000, () => {
