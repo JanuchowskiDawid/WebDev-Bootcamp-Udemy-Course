6ddcde6f9ac79296aebe5eb4e2dea16d2aa48312
Dawid Januchowski <mmalpa97@wp.pl>
2022-09-01 10:17:50 +0200

    created function to get rid off try catch

--- MONGOOSE_EXPRESS/index.js
+++ MONGOOSE_EXPRESS/index.js
@@ -24,74 +24,69 @@ app.use(methodOverride('_method'))
 
 const categories = ['fruit', 'vegetable', 'dairy'];
 
-app.get('/products', async (req, res) => {
+function WrapAsync(fn) {
     try {
-        const { category } = req.query;
-        if (category) {
-            const products = await Product.find({ catergory: category });
-            res.render('./products', { products, category });
-        } else {
-            const products = await Product.find({});
-            res.render('./products', { products, category: 'All' });
-        }
+        fn(req, res, next).catch(e => next(e));
     }
     catch (e) {
         next(e);
     }
-})
+}
+
+app.get('/products', WrapAsync(async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ catergory: category });
+        res.render('./products', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('./products', { products, category: 'All' });
+    }
+}))
 
 app.get('/products/new', (req, res) => {
     res.render('products/new');
 })
 
-app.get('/products/:id/edit', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        if (!product) {
-            throw new AppError('Product Not Found', 404);
-        }
-        res.render('products/show', { product })
-        res.render('products/edit', { product, categories }); s
-    } catch (e) {
-        next(e);
+app.get('/products/:id/edit', WrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        throw new AppError('Product Not Found', 404);
     }
-})
+    res.render('products/show', { product })
+    res.render('products/edit', { product, categories });
+}))
 
 app.post('/products', async (req, res, next) => {
-    try {
-        const newProduct = new Product(req.body);
-        await newProduct.save();
-        res.redirect(`/products/${newProduct._id}`);
-    }
-    catch (e) {
-        next(e)
-    }
-})
 
-app.get('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findById(id);
-        if (!product) {
-            return next(new AppError('Product Not Found', 404));
+    return ((req, res, next) => {
+        try {
+            const newProduct = new Product(req.body);
+            await newProduct.save();
+            res.redirect(`/products/${newProduct._id}`);
         }
-        res.render('products/show', { product })
-    } catch (e) {
-        next(e);
-    }
+        catch (e) {
+            next(e)
+        }
+    })
 })
 
-app.put('/products/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
-        res.redirect(`/products/${id}`);
+
+app.get('/products/:id', WrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        return next(new AppError('Product Not Found', 404));
     }
-    catch (e) {
-        next(e);
-    }
-})
+    res.render('products/show', { product })
+}))
+
+app.put('/products/:id', WrapAsync(async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true });
+    res.redirect(`/products/${id}`);
+}))
 
 app.delete('/products/:id', async (req, res) => {
     const { id } = req.params;
