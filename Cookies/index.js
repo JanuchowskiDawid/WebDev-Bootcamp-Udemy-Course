54d78be99ea9b2530fc68087bb8f5e121de989a2
Dawid Januchowski <mmalpa97@wp.pl>
2022-09-06 18:25:10 +0200

    Retrieving cookies and creating signed cookies

--- Cookies/index.js
+++ Cookies/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
 
+const cookieParser = require('cookie-parser');
+app.use(cookieParser('thisismysecrey'));
+
 app.get('/greet', (req, res) => {
-    res.send("hallo");
+    const { name = 'Noname' } = req.cookies;
+    res.send(`hallo ${name}`);
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true });
+
+    res.send(`hallo`);
+})
+app.get('/verifysignedcookie', (req, res) => {
+    console.log(req.cookies);
+    res.send(req.signedCookies);
 })
 
 app.get('/setname', (req, res) => {
