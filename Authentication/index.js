71580f1069ea9b27716eaf560e153b66f2f13fed
Dawid Januchowski <mmalpa97@wp.pl>
2022-09-08 16:53:07 +0200

    Logging in and out

--- Authentication/index.js
+++ Authentication/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost:27017/Bcrypt', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -14,6 +15,8 @@ mongoose.connect('mongodb://localhost:27017/Bcrypt', { useNewUrlParser: true, us
     })
 
 app.use(express.urlencoded({ extended: true }));
+app.use(session({ secret: 'nosecret' }));
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,20 +24,43 @@ app.set('views', 'views');
 app.get('/register', (req, res) => {
     res.render('register')
 })
+app.get('/login', (req, res) => {
+    res.render('login')
+})
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
     const hash = await bcrypt.hash(password, 12);
     const newUser = new User({ username, password: hash });
     await newUser.save();
+    req.session.user_id = user.id;
     res.redirect('/');
+})
+
+app.post('/login', async (req, res) => {
+    const { password, username } = req.body;
+    const user = await User.findOne({ username: username });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (validPassword) {
+        req.session.user_id = user.id;
+        res.redirect("/secret")
+    } else {
+        res.send('Bad password or login')
+    }
+})
+
+app.post('/logout', (req, res) => {
+    // req.session.user_id = null;
+    req.session.destroy();
+    res.redirect('/login');
 })
 
 app.get('/', (req, res) => {
     res.send('homepage');
 })
 app.get('/secret', (req, res) => {
-    res.send('You must be logged in to see me');
+    if (!req.session.user_id) { res.redirect('/login') };
+    res.render('secret');
 })
 
 app.listen(3000, () => {
